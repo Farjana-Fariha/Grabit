eaffac17c2235d9470429ad0838b1412a377e47a
aburiad <farjanafariha507@gmail.com>
2025-03-29 13:48:49 +0600

    Added carousel to category

--- src/Components/Layouts/Categories/Categories.jsx
+++ src/Components/Layouts/Categories/Categories.jsx
@@ -1,6 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Category from "../Category/Category";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { FreeMode, Pagination } from 'swiper/modules';
+
 const Categories = () => {
   const [categories, setCategories] = useState([]);
   useEffect(() => {
@@ -13,9 +25,21 @@ const Categories = () => {
   }, []);
   return (
    <section className="section-container section-gap flex flex-wrap justify-center gap-x-5 ">
-      {
-         categories.map((category) => <Category></Category>)
+          <Swiper
+        slidesPerView={6}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+            {
+         categories.map((category) => <SwiperSlide><Category data={category}></Category></SwiperSlide>)
       }
+      </Swiper>
+
    </section>
   );
 };
